4b4a7c82fd95110e5df7897a0bdcb6ec6241d629
migo.weng <migo.weng@caloudi.com>
2022-03-11 10:42:19 +0800

    refactor(js-docs): routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { JsDocsComponent } from './component/js-docs/js-docs.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'app-js-docs', pathMatch: 'full' },
+  { path: '', component: JsDocsComponent },
   { path: 'app-js-docs', component: JsDocsComponent },
   { path: '**', component: JsDocsComponent },
 ];
